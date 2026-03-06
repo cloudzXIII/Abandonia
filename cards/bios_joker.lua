9bbdfbc7bbc4203d3c8f58e721e973fa1c317727
cloudzXIII <yusufaslam110@gmail.com>
2026-03-06 18:07:48 +0000

    WIP BIOS JOKER

---
+++ cards/bios_joker.lua
@@ -0,0 +1,22 @@
+SMODS.Joker {
+    key = 'bios_joker',
+
+    loc_vars = function(self, info_queue, card)
+        return { vars = {} }
+    end,
+
+    rarity = 1,
+    atlas = 'AbandoniaJokers',
+    pos = { x = 0, y = 3 },
+    cost = 4,
+    discovered = true,
+    blueprint_compat = true,
+
+    config = { extra = {} },
+
+    calculate = function(self, card, context)
+    end,
+    abn_artist_credits = {
+        artist = "lolhappy909_lol"
+    },
+}
