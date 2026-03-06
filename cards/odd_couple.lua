d2f2b454a0ae54e79c21a270f7dba52b9703e417
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-03-06 23:18:35 +0000

    Odd Couple! (not even?!)

---
+++ cards/odd_couple.lua
@@ -0,0 +1,35 @@
+SMODS.Joker {
+  key = 'odd_couple',
+
+  loc_vars = function(self, info_queue, card)
+    return { vars = {} }
+  end,
+
+  rarity = 3,
+  atlas = 'AbandoniaJokers',
+  pos = { x = 4, y = 6 },
+  cost = 10,
+  discovered = true,
+  blueprint_compat = true,
+
+  config = { extra = {} },
+
+  calculate = function(self, card, context)
+    if context.individual and context.cardarea == G.play and not context.other_card:is_face() then
+      return {
+        mult = context.other_card.base.nominal,
+      }
+    end
+  end,
+  abn_artist_credits = {
+    artist = "Julisnivy",
+  },
+  in_pool = function(self, args)
+    for _, playing_card in ipairs(G.playing_cards or {}) do
+      if playing_card:is_face() then
+        return false
+      end
+    end
+    return true
+  end
+}
