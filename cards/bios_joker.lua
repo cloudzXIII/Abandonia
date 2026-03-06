f6ec3de770e33c203d71649cbef8a4797e4a3ff9
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-03-06 23:18:00 +0000

    Digital Currency! $.$

--- cards/bios_joker.lua
+++ cards/bios_joker.lua
@@ -1,3 +1,4 @@
+--[[ (no effect yet)
 SMODS.Joker {
     key = 'bios_joker',
 
@@ -20,3 +21,4 @@ SMODS.Joker {
         artist = "lolhappy909_lol"
     },
 }
+--]]
---
+++ cards/digital_currency.lua
@@ -0,0 +1,30 @@
+SMODS.Joker {
+  key = 'digital_currency',
+
+  loc_vars = function(self, info_queue, card)
+    return { vars = { card.ability.extra.dollars } }
+  end,
+
+  rarity = 1,
+  atlas = 'AbandoniaJokers',
+  pos = { x = 3, y = 6 },
+  cost = 4,
+  discovered = true,
+  blueprint_compat = true,
+
+  config = { extra = { dollars = 1 } },
+
+  calculate = function(self, card, context)
+    if context.individual and context.cardarea == G.play then
+      context.other_card.ability.perma_p_dollars = (context.other_card.ability.perma_p_dollars or 0) +
+          card.ability.extra.dollars
+      return {
+        message = localize('k_upgrade_ex'),
+        colour = G.C.MONEY
+      }
+    end
+  end,
+  abn_artist_credits = {
+    artist = "DankPastaMaster"
+  },
+}
