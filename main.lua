8d87c2fb78fd2701c00e3e816acd0ae0be5b6edd
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-03-08 09:50:57 +0000

    Inkblot Test! I just dropped it-

--- main.lua
+++ main.lua
@@ -62,4 +62,13 @@ ABN.calculate = function(self, context)
       }
     end
   end
+
+  if context.after and context.main_eval and not context.blueprint then
+    for _, v in ipairs(context.full_hand) do
+      if v.ability.abn_forced_selection or v.abn_inkblot_forced_selection then
+        v.ability.abn_forced_selection = nil
+        v.ability.abn_inkblot_forced_selection = nil
+      end
+    end
+  end
 end
