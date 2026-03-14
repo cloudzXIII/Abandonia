492339a70e9c48fb5fcd4705a87643a3c0464419
cloudzXIII <yusufaslam110@gmail.com>
2026-03-14 10:52:10 +0000

    oops again

--- utilities/value_manipulation.lua
+++ utilities/value_manipulation.lua
@@ -25,7 +25,7 @@ local function multiply_table_values(tbl, multiplier, skip_key)
     for key, value in pairs(tbl) do
         if skip_key and unkeywords[key] then
         elseif type(value) == "number" then
-            if value ~= 0 and value ~= 1 and multiplier > 1 then
+            if value ~= 0 and value ~= 1 then
                 tbl[key] = value * multiplier
             end
         elseif type(value) == "table" then
