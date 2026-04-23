34e8024fb9b42ed7f209f93760db67fe2c74ef55
EricTheToon <EricTheToon@gmail.com>
2026-04-23 07:05:19 -0500

    add penumbra suit

--- content/misc/suits.lua
+++ content/misc/suits.lua
@@ -16,3 +16,22 @@ SMODS.Suit {
     end
   end,
 }
+SMODS.Suit {
+  key = 'Penumbra',
+  card_key = 'PN',
+  lc_atlas = "AbandoniaSuits",
+  hc_atlas = "AbandoniaSuits",
+  lc_ui_atlas = "AbandoniaSuitIcons",
+  hc_ui_atlas = "AbandoniaSuitIcons",
+  pos = { y = 1 },
+  ui_pos = { x = 0, y = 1 },
+  lc_colour = HEX("8570c7"),
+  in_pool = function(self, args)
+    if args and ((args.rank == '') or (args.initial_deck)) then
+      return false
+    else
+      return true
+    end
+  end,
+}
+
