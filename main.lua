e72c495a09d48a7e4ab8060f4b8a0e2c9c178468
EricTheToon <EricTheToon@gmail.com>
2026-04-22 23:58:16 -0500

    penumbra suit

--- main.lua
+++ main.lua
@@ -19,6 +19,7 @@ function loc_colour(_c, _default)
   end
 
   G.ARGS.LOC_COLOURS.abn_snow = HEX("5dc6e2")
+  G.ARGS.LOC_COLOURS.abn_penumbra = HEX("8570c7")
   G.ARGS.LOC_COLOURS.abn_hazard = HEX("831717")
   G.ARGS.LOC_COLOURS.abn_calamity = HEX("c3a37a")
   G.ARGS.LOC_COLOURS.abn_perishable = HEX("687ee7")
