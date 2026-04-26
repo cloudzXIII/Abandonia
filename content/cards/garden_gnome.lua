1b92c67aee61aa6cd4eaf15526c681bd2b71e365
Lact4 <103157629+marcoosgiam@users.noreply.github.com>
2026-04-26 12:13:07 -0300

    Interminable Joker,Garden Gnome,Codding Error

---
+++ content/cards/garden_gnome.lua
@@ -0,0 +1,35 @@
+SMODS.Joker {
+    key = 'garden_gnome',
+    rarity = 1,
+    atlas = 'AbandoniaJokers',
+    pos = { x = 0, y = 12 },
+    cost = 6,
+    discovered = false,
+    blueprint_compat = true,
+    config={
+        extra={
+            chance=10,
+        },
+    },
+    calculate = function(self, card, context)
+        if context.individual and context.cardarea == G.play then
+            if context.other_card:is_suit("Spades") or context.other_card:is_suit("Clubs") then
+                if SMODS.pseudorandom_probability(card,"j_abn_garden_gnome",1,card.ability.extra.chance,"j_abn_garden_gnome") then
+                    context.other_card:set_edition("e_negative")
+                end
+            end
+        end
+    end,
+
+    abn_artist_credits = {
+        artist = "TanTanich",
+    },
+    loc_vars = function(self, info_queue, card)
+        local numerator,denominator = SMODS.get_probability_vars(card,1,card.ability.extra.chance,"j_abn_garden_gnome")
+        return {
+            vars={
+                numerator,denominator
+            },
+        }
+    end,
+}
