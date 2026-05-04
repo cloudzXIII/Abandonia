4048b0fdb45d020dc275207f539298b76005be41
EricTheToon <EricTheToon@gmail.com>
2026-05-03 19:26:57 -0500

    fix sigil tag crash

--- content/misc/tags.lua
+++ content/misc/tags.lua
@@ -222,14 +222,14 @@ SMODS.Tag {
   pos = { x = 1, y = 1 },
   atlas = "AbandoniaTags",
   loc_vars = function(self, info_queue, tag)
-    info_queue[#info_queue + 1] = G.P_CENTERS.p_abn_sigilbooster
+    info_queue[#info_queue + 1] = G.P_CENTERS.p_abn_sigil_normal_1
   end,
   apply = function(self, tag, context)
     if context.type == 'new_blind_choice' then
       local lock = tag.ID
       G.CONTROLLER.locks[lock] = true
       tag:yep('+', G.C.SECONDARY_SET.Spectral, function()
-        local booster = SMODS.create_card { key = 'p_abn_sigilbooster', area = G.play }
+        local booster = SMODS.create_card { key = 'p_abn_sigil_normal_1', area = G.play }
         booster.T.x = G.play.T.x + G.play.T.w / 2 - G.CARD_W * 1.27 / 2
         booster.T.y = G.play.T.y + G.play.T.h / 2 - G.CARD_H * 1.27 / 2
         booster.T.w = G.CARD_W * 1.27
