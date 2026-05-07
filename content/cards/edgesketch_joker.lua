36227c3e803e5a257c4910ca11728b18ac57dc38
Marffe <efframlol@gmail.com>
2026-05-07 14:04:07 -0500

    Edgesketch is now Uncommon and cost 6, no longer scales with itself

--- content/cards/edgesketch_joker.lua
+++ content/cards/edgesketch_joker.lua
@@ -3,10 +3,10 @@ SMODS.Joker {
   loc_vars = function(self, info_queue, card)
     return { vars = { card.ability.extra.mult, card.ability.extra.chips, card.ability.extra.mult_gain, card.ability.extra.chips_gain, } }
   end,
-  rarity = 1,
+  rarity = 2,
   atlas = 'ABNJokerSheet6',
   pos = { x = 9, y = 2 },
-  cost = 4,
+  cost = 6,
   discovered = false,
   blueprint_compat = true,
   eternal_compat = true,
@@ -14,11 +14,11 @@ SMODS.Joker {
   config = { extra = { mult = 0, chips = 0, mult_gain = 5, chips_gain = 15 } },
   calculate = function(self, card, context)
     if context.other_joker and not context.blueprint then
-      if context.other_joker ~= card and context.other_joker == G.jokers.cards[#G.jokers.cards] then
+      if context.other_joker == G.jokers.cards[#G.jokers.cards] then
         card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.mult_gain
         return { message = localize('k_upgrade_ex'), colour = G.C.FILTER, card = card }
       end
-      if context.other_joker ~= card and context.other_joker == G.jokers.cards[1] then
+      if context.other_joker == G.jokers.cards[1] then
         card.ability.extra.chips = card.ability.extra.chips + card.ability.extra.chips_gain
         return { message = localize('k_upgrade_ex'), colour = G.C.FILTER, card = card }
       end
