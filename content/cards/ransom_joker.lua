f25a8fc596e2f45c3b64da22ea7664ed5b1a3001
EricTheToon <EricTheToon@gmail.com>
2026-06-06 09:59:24 -0500

    fix ransom again

--- content/cards/ransom_joker.lua
+++ content/cards/ransom_joker.lua
@@ -1,4 +1,3 @@
-
 SMODS.Joker {
   key = 'ransom_joker',
   rarity = 2,
@@ -16,64 +15,66 @@ SMODS.Joker {
   
   calculate = function(self, card, context)
     if context.end_of_round and context.game_over == false and context.main_eval and not context.blueprint then
-	  card.ability.extra_value = card.ability.extra_value + (#G.jokers.cards * card.ability.extra.dollars)
-	  card:set_cost()
+      card.ability.extra_value = card.ability.extra_value + (#G.jokers.cards * card.ability.extra.dollars)
+      card:set_cost()
       return {
-		message = localize('k_val_up'),
-		dollars = #G.jokers.cards*-card.ability.extra.dollars,
-		card = card,
+        message = localize('k_val_up'),
+        dollars = #G.jokers.cards * -card.ability.extra.dollars,
+        card = card,
       }
     end
 	
-	local triggered_card = context.selling_card or context.destroyed_card
-  
-	if triggered_card and triggered_card.ability.set == 'Joker' then
-		local rarity = triggered_card.config.center.rarity
-		if SMODS.pseudorandom_probability(card, 'ransom', 1, card.ability.extra.odds) then
-			if rarity == 1 then
-				SMODS.add_card {
-				set = "Joker",
-				rarity = "Uncommon",
-				key_append = "abn_ransom"
-				}
-			elseif rarity == 2 then
-				SMODS.add_card {
-				set = "Joker",
-				rarity = "Rare",
-				key_append = "abn_ransom"
-				}
-			elseif rarity == 3 then
-				SMODS.add_card {
-				set = "Joker",
-				rarity = "Legendary",
-				key_append = "abn_ransom"
-				}
-			elseif rarity == 4 then
-				SMODS.add_card {
-				set = "Joker",
-				rarity = "abn_SuperRare",
-				key_append = "abn_ransom"
-				}
-			elseif rarity == "abn_SuperRare" then
-				SMODS.add_card {
-				set = "Joker",
-				rarity = "abn_ParallelRare",
-				key_append = "abn_ransom"
-				}
-			elseif rarity == "abn_ParallelRare" then
-				SMODS.add_card {
-				set = "Joker",
-				rarity = "abn_ParallelRare",
-				key_append = "abn_ransom"
-				}
-			end
-		else
-			card_eval_status_text(card, 'extra', nil, nil, nil, {
-				message = localize('k_nope_ex'),
-				colour = G.C.SECONDARY_SET.Tarot,
-			})
-		end
-	end
+    if context.selling_card or context.destroyed_card then
+      local triggered_card = context.card
+      
+      if triggered_card and triggered_card.ability and triggered_card.ability.set == 'Joker' then
+        local rarity = triggered_card.config.center.rarity
+        if SMODS.pseudorandom_probability(card, 'ransom', 1, card.ability.extra.odds) then
+          if rarity == 1 then
+            SMODS.add_card {
+              set = "Joker",
+              rarity = "Uncommon",
+              key_append = "abn_ransom"
+            }
+          elseif rarity == 2 then
+            SMODS.add_card {
+              set = "Joker",
+              rarity = "Rare",
+              key_append = "abn_ransom"
+            }
+          elseif rarity == 3 then
+            SMODS.add_card {
+              set = "Joker",
+              rarity = "Legendary",
+              key_append = "abn_ransom"
+            }
+          elseif rarity == 4 then
+            SMODS.add_card {
+              set = "Joker",
+              rarity = "abn_SuperRare",
+              key_append = "abn_ransom"
+            }
+          elseif rarity == "abn_SuperRare" then
+            SMODS.add_card {
+              set = "Joker",
+              rarity = "abn_ParallelRare",
+              key_append = "abn_ransom"
+            }
+          elseif rarity == "abn_ParallelRare" then
+            SMODS.add_card {
+              set = "Joker",
+              rarity = "abn_ParallelRare",
+              key_append = "abn_ransom"
+            }
+          end
+        else
+          card_eval_status_text(card, 'extra', nil, nil, nil, {
+            message = localize('k_nope_ex'),
+            colour = G.C.SECONDARY_SET.Tarot,
+          })
+        end
+      end
+    end
   end,
   
   abn_artist_credits = {
