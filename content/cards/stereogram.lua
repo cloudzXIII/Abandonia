efbf917ced1baae4f44baab1c8fa91c15a524337
EricTheToon <EricTheToon@gmail.com>
2026-06-22 07:37:37 -0500

    fix stereogram

--- content/cards/stereogram.lua
+++ content/cards/stereogram.lua
@@ -11,40 +11,16 @@ SMODS.Joker {
         return { vars = { card.ability.extra.balance } }
     end,
     calculate = function(self, card, context)
-        -- 1. Check if individual context is even reaching the joker
         if context.individual and context.cardarea == G.play then
             local other_card = context.other_card
-            local id = other_card:get_id()
-            print("[Stereogram] Fired for card ID: " .. tostring(id))
             
-            local rank_key = nil
-            -- Scan the object buffer for the rank
-            for _, key in ipairs(SMODS.Rank.obj_buffer) do
-                local rank = SMODS.Ranks[key]
-                if rank and rank.id == id then
-                    rank_key = key
-                    break
-                end
-            end
-            
-            print("[Stereogram] Found Rank Key: " .. tostring(rank_key))
-            
-            if rank_key then
-                local rank = SMODS.Ranks[rank_key]
-                -- Check face card and Ace logic
-                print("[Stereogram] Is Face: " .. tostring(rank.face) .. " | Is Ace: " .. tostring(rank_key == 'Ace'))
+            if other_card:get_id() ~= 14 and not other_card:is_face() then
                 
-                if rank_key ~= 'Ace' and not rank.face then
-                    print("[Stereogram] SUCCESS: Card is a number rank! Returning abn_balance_percent = " .. tostring(card.ability.extra.balance))
-                    return {
-                        abn_balance_percent = card.ability.extra.balance,
-                        card = card
-                    }
-                else
-                    print("[Stereogram] EXCLUDED: Card is a face card or an Ace.")
-                end
-            else
-                print("[Stereogram] ERROR: Could not map card ID to any SMODS rank.")
+                ABN.balance_percent(other_card, (card.ability.extra.balance * 0.01))
+
+                return {
+                    card = other_card
+                }
             end
         end
     end,
