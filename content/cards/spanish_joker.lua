cdf946f88fd2aeabb73ec1f84a5648e32f030665
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-06-23 00:06:04 +0100

    add infoqueue

--- content/cards/spanish_joker.lua
+++ content/cards/spanish_joker.lua
@@ -7,20 +7,20 @@ SMODS.Joker {
   discovered = false,
   blueprint_compat = true,
 
-  config = { extra = { mult = 0, multadd = 1} },
+  config = { extra = { mult = 0, multadd = 1 } },
 
   loc_vars = function(self, info_queue, card)
-    return { vars = { card.ability.extra.mult, card.ability.extra.multadd} }
+    info_queue[#info_queue + 1] = G.P_CENTERS.e_abn_vintage
+    return { vars = { card.ability.extra.mult, card.ability.extra.multadd } }
   end,
 
   calculate = function(self, card, context)
-  
     if context.joker_main then
       local triggered = false
 
       for i = 1, #context.scoring_hand do
         local scoring_card = context.scoring_hand[i]
-        
+
         if scoring_card:is_suit("Hearts") then
           scoring_card:change_suit("abn_Snow")
           triggered = true
@@ -35,23 +35,23 @@ SMODS.Joker {
           triggered = true
         end
       end
-	  
-	  return {
-		mult = card.ability.extra.mult,
-	  }
+
+      return {
+        mult = card.ability.extra.mult,
+      }
     end
-	
-	if context.change_suit and context.other_card then
-	  if card.edition and card.edition.key == "e_abn_vintage" then
-		card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.multadd
-		card.ability.extra_value = card.ability.extra_value + card.ability.extra.multadd
-		card:set_cost()
-		return {
-			message = localize('k_upgrade_ex'),
-			colour = G.C.MULT,
-			card = card
-		}
-	 end
+
+    if context.change_suit and context.other_card then
+      if card.edition and card.edition.key == "e_abn_vintage" then
+        card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.multadd
+        card.ability.extra_value = card.ability.extra_value + card.ability.extra.multadd
+        card:set_cost()
+        return {
+          message = localize('k_upgrade_ex'),
+          colour = G.C.MULT,
+          card = card
+        }
+      end
     end
   end,
 
