1aad35da4eaae96b092b3cb32b949a9f86cc4a76
EricTheToon <EricTheToon@gmail.com>
2026-06-23 10:51:46 -0500

    joker reworks

--- content/cards/hawk.lua
+++ content/cards/hawk.lua
@@ -1,9 +1,9 @@
--- Hawk (coded by cloudzXIII)
 SMODS.Joker {
   key = 'hawk',
 
   loc_txt = {
     ['en-us'] = {
+      name = "Hawk",
       unlock = {
         "?????",
       },
@@ -11,7 +11,7 @@ SMODS.Joker {
   },
   loc_vars = function(self, info_queue, card)
     local cae = card.ability.extra
-    return { vars = { cae.x_mult, cae.x_mult_gain } }
+    return { vars = { cae.xmult, cae.xchips, cae.xmultadd, cae.xchipsadd, cae.mult, cae.chips, } }
   end,
 
   rarity = 4,
@@ -23,28 +23,63 @@ SMODS.Joker {
   blueprint_compat = true,
   unlocked = false,
 
-  config = { extra = { x_mult = 1, x_mult_gain = 0.05 } },
+  config = { extra = { xmult = 1, xchips = 1, xmultadd = 1.16, xchipsadd = 1.13, mult = 4, chips = 11, } },
 
   calculate = function(self, card, context)
-    if context.individual and context.cardarea == G.play and context.other_card:is_suit('abn_Penumbra') then
-      if not context.blueprint then
-        SMODS.scale_card(card, {
-          ref_table = card.ability.extra,
-          ref_value = "x_mult",
-          scalar_value = "x_mult_gain",
-        })
+    if context.before and not context.blueprint then
+      local first_card = context.scoring_hand[1]
+      
+      if first_card then
+        if ABN.is_even(first_card) then
+          card.ability.extra.xmult = card.ability.extra.xmult * card.ability.extra.xmultadd
+          card_eval_status_text(card, 'extra', nil, nil, nil, { message = localize('k_upgrade_ex'), })
+        elseif ABN.is_odd(first_card) then
+          card.ability.extra.xchips = card.ability.extra.xchips * card.ability.extra.xchipsadd
+          card_eval_status_text(card, 'extra', nil, nil, nil, { message = localize('k_upgrade_ex'), })
+        end
+
+        for i = 1, #context.scoring_hand do
+          local scoring_card = context.scoring_hand[i]
+          if ABN.is_even(first_card) then
+            scoring_card.ability.perma_mult = (scoring_card.ability.perma_mult or 0) + card.ability.extra.mult
+          elseif ABN.is_odd(first_card) then
+            scoring_card.ability.perma_bonus = (scoring_card.ability.perma_bonus or 0) + card.ability.extra.chips
+          end
+        end
       end
+    end
+    
+    if context.destroy_card and context.scoring_hand and context.scoring_hand[1] then
+      if context.destroy_card == context.scoring_hand[1] then
+        return { remove = true }
+      end
+    end
+
+    if context.joker_main then
       return {
-        x_mult = card.ability.extra.x_mult,
-        card = card,
-        colour = G.C.RED
+        xmult = card.ability.extra.xmult,
+        xchips = card.ability.extra.xchips,
       }
     end
-    if context.after and (G.GAME.chips + SMODS.calculate_round_score()) > G.GAME.blind.chips then
-      for _, scored_card in ipairs(context.scoring_hand) do
-        card:juice_up(0.3, 0.5)
 
-        ABN.level_up_rank(scored_card, scored_card.base.value)
+    if context.final_scoring_step and not context.blueprint and context.scoring_hand then
+      if SMODS.calculate_round_score() + G.GAME.chips >= G.GAME.blind.chips then
+        local has_odd = false
+        local has_even = false
+        
+        for i = 1, #context.scoring_hand do
+          if ABN.is_odd(context.scoring_hand[i]) then has_odd = true end
+          if ABN.is_even(context.scoring_hand[i]) then has_even = true end
+        end
+
+        if has_odd and has_even then
+          for i = 1, #context.scoring_hand do
+            local r_val = context.scoring_hand[i].base.value
+            if r_val then
+              ABN.level_up_rank(card, r_val, 1)
+            end
+          end
+        end
       end
     end
   end,
@@ -56,12 +91,4 @@ SMODS.Joker {
   abn_artist_credits = {
     artist = "IPreferCheddar",
   },
-  in_pool = function(self)
-    for _, card in ipairs(G.playing_cards or {}) do
-      if card:is_suit("abn_Penumbra") then
-        return true
-      end
-    end
-    return false
-  end,
 }
--- content/cards/inferno_comedian.lua
+++ content/cards/inferno_comedian.lua
@@ -44,28 +44,22 @@ SMODS.Joker {
   end,
 
   calculate = function(self, card, context)
-    -- 1. XMult from played cards individually
     if context.individual and context.cardarea == G.play then
       return {
         xmult = card.ability.extra.xmult,
       }
     end
 	
-    -- 2. Regular flat Mult evaluation during final scoring
     if context.joker_main then
       return {
         mult = card.ability.extra.mult
       }
     end
 
-    -- 3. Scaling logic: when a playing card is destroyed/removed
     if context.remove_playing_cards and not context.blueprint then
       local gained_mult = 0
       for _, removed_card in ipairs(context.removed) do
-        -- Check if the destroyed card specifically has the Oilfire enhancement
         if removed_card.config.center == G.P_CENTERS.m_abn_oilfire then
-          -- Safely fetch nominal chip value (Aces = 11, Face = 10, etc.) 
-          -- Falls back to 0 if it has no rank (like a Stone card)
           local card_value = (not SMODS.has_no_rank(removed_card)) and removed_card.base.nominal or 0
           gained_mult = gained_mult + card_value
         end
@@ -74,13 +68,12 @@ SMODS.Joker {
       if gained_mult > 0 then
         card.ability.extra.mult = card.ability.extra.mult + gained_mult
         return {
-			message = localize('k_upgrade_ex'),
-			colour = G.C.MULT,
-            card = card
+          message = localize('k_upgrade_ex'),
+          colour = G.C.MULT,
+          card = card
         }
       end
     end
-
 
     if context.final_scoring_step and not context.repetition then
       local blind_chips = G.GAME.blind and G.GAME.blind.chips or 0
