d774c98052fdf64c209e1c0d2df628dc29f1c239
EricTheToon <EricTheToon@gmail.com>
2026-06-23 15:13:25 -0500

    fix reel giving emult instead of echips

--- content/misc/editions.lua
+++ content/misc/editions.lua
@@ -267,7 +267,7 @@ SMODS.Edition {
   calculate = function(self, card, context)
     if context.pre_joker or (context.main_scoring and context.cardarea == G.play) then
       return {
-        emult = card.edition.echips
+        echips = card.edition.echips
       }
     end
   end,
