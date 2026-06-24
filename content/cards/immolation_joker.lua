f9b91083db8a8949785ecc15f99e3cb4a7e420ea
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-06-24 21:21:54 +0100

    add tooltip to immolation joker
    also uses `enhancement_gate` instead of in_pool check

--- content/cards/immolation_joker.lua
+++ content/cards/immolation_joker.lua
@@ -1,24 +1,16 @@
 SMODS.Joker {
   key = 'immolation_joker',
-  rarity = 2, 
-  atlas = 'ABNJokerSheet1', 
-  pos = { x = 8, y = 5 }, 
+  rarity = 2,
+  atlas = 'ABNJokerSheet1',
+  pos = { x = 8, y = 5 },
   cost = 6,
   discovered = false,
   blueprint_compat = false,
 
-  in_pool = function(self)
-    if not G.playing_cards then return false end
-
-	for _, card in ipairs(G.playing_cards) do
-		if card and card.config and card.config.center then
-			if card.config.center == G.P_CENTERS.m_abn_oilfire then
-				return true
-			end
-		end
-	end
-	return false
+  loc_vars = function(self, info_queue, card)
+    info_queue[#info_queue + 1] = G.P_CENTERS.m_abn_oilfire
   end,
+  enhancement_gate = "m_abn_oilfire",
 
   abn_artist_credits = {
     artist = "Vlambambo",
