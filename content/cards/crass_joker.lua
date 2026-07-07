a10f65d978233fc529c4795d57118716b0e3de36
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-07-07 23:49:31 +0100

    crash fix in collection

--- content/cards/crass_joker.lua
+++ content/cards/crass_joker.lua
@@ -10,27 +10,27 @@ SMODS.Joker {
     abn_coder = "LasagnaFelidae",
     config = { extra = { chips = 0, chips_mod = 1 } },
     loc_vars = function(self, info_queue, card)
-        return { 
-            vars = { 
-                card.ability.extra.chips_mod, card.ability.extra.chips, 
-            } 
+        return {
+            vars = {
+                card.ability.extra.chips_mod, card.ability.extra.chips,
+            }
         }
     end,
-  
+
     calculate = function(self, card, context)
         if context.buying_card and context.card.edition and not context.blueprint then
-            card.ability.extra.chips = card.ability.extra.chips + (card.ability.extra.chips_mod * (context.card.cost > 0 and context.card.cost or context.card.base_cost))
+            card.ability.extra.chips = card.ability.extra.chips +
+                (card.ability.extra.chips_mod * (context.card.cost > 0 and context.card.cost or context.card.base_cost))
         end
         if context.joker_main then
             return {
                 chips = card.ability.extra.chips
             }
-		end
-
+        end
     end,
 
     in_pool = function(self, args)
-        for i, joker in ipairs(G.jokers.cards) do
+        for i, joker in ipairs(G.jokers.cards or {}) do
             if joker.edition then
                 return true
             end
@@ -38,8 +38,8 @@ SMODS.Joker {
         return false
     end,
 
-    
-    
+
+
     abn_artist_credits = {
         artist = "DOS",
     },
