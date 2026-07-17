98fe8fe71f3d3ce0aae07f3e49c94b0743589bd4
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-07-18 00:59:46 +0100

    context.starting.shop :wilted_rose:
    oop

--- content/cards/mutually_assured_destruction.lua
+++ content/cards/mutually_assured_destruction.lua
@@ -7,14 +7,13 @@ SMODS.Joker {
     discovered = false,
     blueprint_compat = false,
     abn_coder = "LasagnaFelidae",
-    loc_vars = function(self,info_queue, card)
+    loc_vars = function(self, info_queue, card)
         local color = G.C.RED
         local text = "Unsafe"
         if card.ability.purchased then
             color, text = G.C.GREEN, "Safe"
         end
-        return {vars = {text, colours = {color}}}
-
+        return { vars = { text, colours = { color } } }
     end,
 
     add_to_deck = function(self, card, from_debuff)
@@ -23,15 +22,15 @@ SMODS.Joker {
         G.GAME.abn_parallelrare_rate = (G.GAME.abn_parallelrare_rate or 0) + 0.005
         G.GAME.abn_virusrare_rate = (G.GAME.abn_virusrare_rate or 0) + 0.0005
     end,
-    remove_from_deck = function(self,card,from_debuff)
+    remove_from_deck = function(self, card, from_debuff)
         G.GAME.abn_legendary_rate = (G.GAME.abn_legendary_rate or 0) - 0.001
         G.GAME.abn_superrare_rate = (G.GAME.abn_superrare_rate or 0) - 0.005
         G.GAME.abn_parallelrare_rate = (G.GAME.abn_parallelrare_rate or 0) - 0.005
         G.GAME.abn_virusrare_rate = (G.GAME.abn_virusrare_rate or 0) - 0.0005
     end,
-    
+
     calculate = function(self, card, context)
-        if context.starting.shop then
+        if context.starting_shop then
             if card.ability.purchased == true then
                 card.ability.purchased = nil
             end
@@ -42,14 +41,11 @@ SMODS.Joker {
         if context.ending_shop and card.ability.purchased == nil then
             SMODS.destroy_cards(G.jokers.cards, true)
         end
-
-
     end,
 
 
-    
+
     abn_artist_credits = {
         artist = "Firch",
     },
 }
-
