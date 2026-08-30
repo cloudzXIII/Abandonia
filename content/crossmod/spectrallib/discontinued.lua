27a122380b878fa2884fbd150b8608a26a4a84c2
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-08-30 20:36:22 +0100

    Discontinued Enhancement fix

--- content/crossmod/spectrallib/discontinued.lua
+++ content/crossmod/spectrallib/discontinued.lua
@@ -1,15 +1,15 @@
 --track unique tags
 local original_add_tag = add_tag
 function add_tag(tag, immediate)
-    if tag and tag.key then
-        G.GAME.abn_SeenTags = G.GAME.abn_SeenTags or {}
-        if not G.GAME.abn_SeenTags[tag.key] then
-            G.GAME.abn_SeenTags[tag.key] = true
-            G.GAME.abn_UniqueTags = (G.GAME.abn_UniqueTags or 0) + 1
-        end
+  if tag and tag.key then
+    G.GAME.abn_SeenTags = G.GAME.abn_SeenTags or {}
+    if not G.GAME.abn_SeenTags[tag.key] then
+      G.GAME.abn_SeenTags[tag.key] = true
+      G.GAME.abn_UniqueTags = (G.GAME.abn_UniqueTags or 0) + 1
     end
+  end
 
-    original_add_tag(tag, immediate)
+  original_add_tag(tag, immediate)
 end
 
 SMODS.Enhancement({
@@ -19,18 +19,18 @@ SMODS.Enhancement({
   config = { extra = { ascension = 0, ascensionadd = 0.10, chips = 0, chipsadd = 10, } },
   loc_vars = function(self, info_queue, card)
     local cae = card.ability.extra
-    return { vars = { cae.ascension, cae.chips, cae.ascensionadd, cae.chipsadd} }
+    local unique_tags = (G.GAME.abn_UniqueTags or 0)
+    local asc = cae.ascensionadd * unique_tags
+    local chips = cae.chipsadd * unique_tags
+    return { vars = { asc, chips, cae.ascensionadd, cae.chipsadd } }
   end,
-  
+
   calculate = function(self, card, context)
-	local cae = card.ability.extra
-	cae.ascension = cae.ascensionadd * (G.GAME.abn_UniqueTags or 0)
-	cae.chips = cae.chipsadd * (G.GAME.abn_UniqueTags or 0)
     if context.main_scoring and context.cardarea == G.play then
-		return {
-          asc = cae.ascension,
-		  chips = cae.chips,
-        }
+      return {
+        asc = card.ability.extra.ascensionadd * (G.GAME.abn_UniqueTags or 0),
+        chips = card.ability.extra.chipsadd * (G.GAME.abn_UniqueTags or 0),
+      }
     end
   end,
   abn_artist_credits = {
