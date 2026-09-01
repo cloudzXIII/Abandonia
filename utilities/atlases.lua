a641e7ca365fb8526601d358b61f55b02cb5690b
EricTheToon <EricTheToon@gmail.com>
2026-09-01 11:46:04 -0500

    Add files via upload

--- utilities/atlases.lua
+++ utilities/atlases.lua
@@ -397,6 +397,13 @@ SMODS.Atlas({
   py = 95,
 })
 
+SMODS.Atlas({
+  key = "AbandoniaStamps",
+  path = "stamps.png",
+  px = 71,
+  py = 95,
+})
+
 --#region Jokers
 SMODS.Atlas({
   key = "ABNJokerSheet1",
