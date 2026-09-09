da9bedae611b3d7e05bcb6ad52f4cdf99af4cd4f
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-09-09 18:35:31 +0100

    some relocalising and bugfixes

--- utilities/atlases.lua
+++ utilities/atlases.lua
@@ -254,6 +254,12 @@ SMODS.Atlas({
   px = 71,
   py = 95,
 })
+SMODS.Atlas({
+  key = "AbandoniaAtomic",
+  path = "atomic.png",
+  px = 71,
+  py = 95,
+})
 
 SMODS.Atlas({
   key = "AbandoniaWeatherReport",
