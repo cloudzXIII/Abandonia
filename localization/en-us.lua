1f5b9a68370570b52a9238ed68075866434d1d32
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-09-11 20:36:47 +0100

    Kiddy Packs!

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -12270,6 +12270,134 @@ return {
       }
     },
     Other = {
+      --#region Kiddy Packs
+      p_abn_standard_kiddy = {
+        name = "Kiddy Standard Pack",
+        text = {
+          "Choose {C:attention}#1#{} of up to",
+          "{C:attention}#2#{C:attention} Playing{} cards to",
+          "add to your deck",
+        }
+      },
+      p_abn_arcana_kiddy = {
+        name = "Kiddy Arcana Pack",
+        text = {
+          "Choose {C:attention}#1#{} of up to",
+          "{C:attention}#2#{C:tarot} Tarot{} cards to",
+          "be used immediately",
+        }
+      },
+      p_abn_spectral_kiddy = {
+        name = "Kiddy Spectral Pack",
+        text = {
+          "Choose {C:attention}#1#{} of up to",
+          "{C:attention}#2#{C:spectral} Spectral{} cards to",
+          "be used immediately",
+        }
+      },
+      p_abn_celestial_kiddy = {
+        name = "Kiddy Celestial Pack",
+        text = {
+          "Choose {C:attention}#1#{} of up to",
+          "{C:attention}#2#{C:planet} Planet{} cards to",
+          "be used immediately",
+        }
+      },
+      p_abn_astro_kiddy = {
+        name = 'Kiddy Astro Pack',
+        text = {
+          'Choose {C:attention}#1#{} of up to',
+          '{C:attention}#2#{} {C:astro_cards}Astro{} cards to',
+          'be used immediately'
+        }
+      },
+      p_abn_sigil_kiddy = {
+        name = 'Kiddy Sigil Pack',
+        text = {
+          'Choose {C:attention}#1#{} of up to',
+          '{C:attention}#2#{} {C:sigils}Sigil{} cards to',
+          'be used immediately',
+          '{C:inactive}(cannot be skipped)'
+        }
+      },
+      p_abn_weather_kiddy = {
+        name = 'Kiddy Forecast Pack',
+        text = {
+          'Choose {C:attention}#1#{} of up to',
+          '{C:attention}#2#{} {C:weather_report}Weather Reports{}',
+        }
+      },
+      p_abn_lexica_kiddy = {
+        name = 'Kiddy Lexica Pack',
+        text = {
+          'Choose {C:attention}#1#{} of up to',
+          '{C:attention}#2#{} {C:lexica}Lexica{} cards'
+        }
+      },
+      p_abn_continent_kiddy = {
+        name = 'Kiddy Continent Pack',
+        text = {
+          'Choose {C:attention}#1#{} of up to',
+          '{C:attention}#2#{} {C:continent}Continent{} cards'
+        }
+      },
+      p_abn_solid_state_kiddy = {
+        name = 'Kiddy Solid State Pack',
+        text = {
+          'Choose {C:attention}#1#{} of up to',
+          '{C:attention}#2#{} {C:solid_state}Solid State{} cards to',
+          'be used immediately'
+        }
+      },
+      p_abn_program_kiddy = {
+        name = 'Kiddy Program Pack',
+        text = {
+          'Choose {C:attention}#1#{} of up to',
+          '{C:attention}#2#{} {C:program_pack}Program{} cards to',
+          'be used immediately'
+        }
+      },
+      p_abn_calligraphy_kiddy = {
+        name = "Kiddy Calligraphy Pack",
+        text = {
+          "Choose {C:attention}#1#{} of up to",
+          "{C:attention}#2#{C:calligraphy} Calligraphy{} cards to",
+          "be used immediately",
+        },
+      },
+      p_abn_glyph_kiddy = {
+        name = "Kiddy Glyph Pack",
+        text = {
+          "Choose {C:attention}#1#{} of up to",
+          "{C:attention}#2# {C:glyphs}Glyph{} cards to",
+          "be used immediately",
+        },
+      },
+      p_abn_calamity_kiddy = {
+        name = 'Kiddy Calamity Pack',
+        text = {
+          'Choose {C:attention}#1#{} of up to',
+          '{C:attention}#2#{} {C:abn_calamity}Calamity{} cards to',
+          'be used immediately'
+        }
+      },
+      p_abn_illusion_kiddy = {
+        name = 'Illusion Pack',
+        text = {
+          'Choose {C:attention}#1#{} of up to',
+          '{C:attention}#2#{} {C:illusion}Illusion{} cards to',
+          'be used immediately'
+        }
+      },
+      p_abn_crimson_kiddy = {
+        name = "Kiddy Crimson Pack",
+        text = {
+          "Choose {C:attention}#1#{} of up to",
+          "{C:attention}#2# {C:crimson}Crimson{} cards to",
+          "be used immediately",
+        },
+      },
+      --#endregion
       abn_empty_stamp = {
         name = "Empty Stamp",
         text = {
@@ -12958,7 +13086,7 @@ return {
       p_abn_sigil_normal = {
         name = 'Sigil Pack',
         text = {
-          'Select {C:attention}#1#{} of',
+          'Choose {C:attention}#1#{} of up to',
           '{C:attention}#2#{} {C:sigils}Sigil{} cards to',
           'be used immediately',
           '{C:inactive}(cannot be skipped)'
@@ -12967,7 +13095,7 @@ return {
       p_abn_sigil_jumbo = {
         name = 'Jumbo Sigil Pack',
         text = {
-          'Select {C:attention}#1#{} of',
+          'Choose {C:attention}#1#{} of up to',
           '{C:attention}#2#{} {C:sigils}Sigil{} cards to',
           'be used immediately',
           '{C:inactive}(cannot be skipped)'
@@ -12976,7 +13104,7 @@ return {
       p_abn_sigil_mega = {
         name = 'Mega Sigil Pack',
         text = {
-          'Select {C:attention}#1#{} of',
+          'Choose {C:attention}#1#{} of up to',
           '{C:attention}#2#{} {C:sigils}Sigil{} cards to',
           'be used immediately',
           '{C:inactive}(cannot be skipped)'
@@ -16064,6 +16192,25 @@ return {
       k_abn_asteroid = "Asteroid",
       k_abn_tenalp = "Tenalp",
       k_abn_split = "Split Planet",
+
+      -- kiddy pack thingys
+      k_standard_kiddy = "Kiddy Standard Pack",
+      k_arcana_kiddy = "Kiddy Arcana Pack",
+      k_spectral_kiddy = "Kiddy Spectral Pack",
+      k_celestial_kiddy = "Kiddy Celestial Pack",
+      k_astro_kiddy = "Kiddy Astro Pack",
+      k_sigil_kiddy = "Kiddy Sigil Pack",
+      k_weather_kiddy = "Kiddy Forecast Pack",
+      k_lexica_kiddy = "Kiddy Lexica Pack",
+      k_continent_kiddy = "Kiddy Continent Pack",
+      k_solid_state_kiddy = "Kiddy Solid State Pack",
+      k_program_kiddy = "Kiddy Program Pack",
+      k_calligraphy_kiddy = "Kiddy Calligraphy Pack",
+      k_glyph_kiddy = "Kiddy Glyph Pack",
+      k_calamity_kiddy = "Kiddy Calamity Pack",
+      k_crimson_kiddy = "Kiddy Crimson Pack",
+
+
 
       -- Consumable Types
       b_weather_report_cards = "Weather Reports",
