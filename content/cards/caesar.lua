626a7fec355190832e5dd4e2bba1e237da72036f
EricTheToon <EricTheToon@gmail.com>
2026-09-10 23:10:03 -0500

    Add files via upload

--- content/cards/caesar.lua
+++ content/cards/caesar.lua
@@ -9,14 +9,14 @@ SMODS.Joker {
   discovered = false,
   blueprint_compat = true,
 
-  config = { extra = { chips = 0, x_mult = 1, heart_chips = 5, heart_mult = 2, x_mult_gain = 0.1, chips_gain = 40 } },
+  config = { extra = { chips = 0, x_mult = 1, crown_chips = 5, crown_mult = 2, x_mult_gain = 0.1, chips_gain = 40 } },
   loc_vars = function(self, info_queue, card)
     local cae = card.ability.extra
-    return { vars = { cae.chips_gain, cae.x_mult_gain, cae.chips, cae.x_mult, cae.heart_chips, cae.heart_mult } }
+    return { vars = { cae.chips_gain, cae.x_mult_gain, cae.chips, cae.x_mult, cae.crown_chips, cae.crown_mult } }
   end,
 
   calculate = function(self, card, context)
-    if context.individual and context.cardarea == G.play and context.other_card:is_suit("Hearts") then
+    if context.individual and context.cardarea == G.play and context.other_card:is_suit("abn_Crown") then
       local continent_count = 0
       for _, v in ipairs(G.consumeables.cards) do
         if v.ability.set == "continent" then
@@ -25,9 +25,9 @@ SMODS.Joker {
       end
       if continent_count > 0 then
         context.other_card.ability.perma_bonus = (context.other_card.ability.perma_bonus or 0) +
-            (card.ability.extra.heart_chips * continent_count)
+            (card.ability.extra.crown_chips * continent_count)
         context.other_card.ability.perma_mult = (context.other_card.ability.perma_mult or 0) +
-            (card.ability.extra.heart_mult * continent_count)
+            (card.ability.extra.crown_mult * continent_count)
         SMODS.calculate_effect({ message = localize("k_upgrade_ex") }, context.other_card)
       end
     end
@@ -58,8 +58,20 @@ SMODS.Joker {
   abn_artist_credits = {
     artist = "hehey",
   },
+  
   in_pool = function(self)
-    return G.GAME.consumeable_usage_total and G.GAME.consumeable_usage_total.continent and
-        G.GAME.consumeable_usage_total.continent > 0
+    local has_used_continent = G.GAME.consumeable_usage_total and G.GAME.consumeable_usage_total.continent and G.GAME.consumeable_usage_total.continent > 0
+  
+    local has_crown_card = false
+    if G.playing_cards then
+      for _, v in ipairs(G.playing_cards) do
+        if v:is_suit("abn_Crown") then
+          has_crown_card = true
+          break
+        end
+      end
+    end
+
+    return has_used_continent and has_crown_card
   end
 }
--- content/cards/cloten.lua
+++ content/cards/cloten.lua
@@ -10,10 +10,11 @@ SMODS.Joker {
     blueprint_compat = true,
 
     calculate = function(self, card, context)
-        if context.individual and context.cardarea == G.play then
+        if context.repetition and context.cardarea == G.play then
             if SMODS.has_enhancement(context.other_card, "m_bonus") then
                 return {
-                    xchips = context.other_card:get_id()
+                    xchips = context.other_card:get_id(),
+					repetitions = 1,
                 }
             end
         end
