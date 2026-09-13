4308b98469bb33932130b6a1420aae68674d843d
EricTheToon <EricTheToon@gmail.com>
2026-09-13 11:21:30 -0500

    new vouchers

--- content/misc/vouchers.lua
+++ content/misc/vouchers.lua
@@ -413,3 +413,240 @@ SMODS.Voucher({
 	cost = 10,
 	requires = { "v_abn_ink_and_quill" },
 })
+
+SMODS.Voucher({
+	key = "magnet",
+	atlas = "AbandoniaVouchers",
+	pos = {
+		x = 1,
+		y = 3,
+	},
+	cost = 10,
+})
+
+SMODS.Voucher({
+	key = "electromagnet",
+	atlas = "AbandoniaVouchers",
+	pos = {
+		x = 2,
+		y = 3,
+	},
+	cost = 10,
+	requires = { "v_abn_magnet" },
+})
+
+SMODS.Voucher({
+    key = "pattern",
+    atlas = "AbandoniaVouchers",
+    pos = {
+        x = 3,
+        y = 3,
+    },
+    cost = 10,
+
+    calculate = function(self, card, context)
+        if context.setting_blind then
+            local most_used_key = nil
+            local highest_uses = -1
+
+            local profile_stats = nil
+            if G.PROFILES and G.SETTINGS and G.SETTINGS.profile and G.PROFILES[G.SETTINGS.profile] then
+                profile_stats = G.PROFILES[G.SETTINGS.profile].consumeable_usage
+            elseif G.PROFILE then
+                profile_stats = G.PROFILE.consumeable_usage
+            end
+
+            for _, center in pairs(G.P_CENTERS) do
+                if center.set == "Tarot" or center.set == "Planet" then
+                    local curr_uses = 0
+                    if profile_stats and profile_stats[center.key] then
+                        curr_uses = profile_stats[center.key].count or profile_stats[center.key] or 0
+                    end
+
+                    if curr_uses > highest_uses then
+                        highest_uses = curr_uses
+                        most_used_key = center.key
+                    end
+                end
+            end
+
+            if not most_used_key or highest_uses <= 0 then
+                most_used_key = "c_fool"
+            end
+
+            local card_type = "Tarot"
+            if G.P_CENTERS[most_used_key] and G.P_CENTERS[most_used_key].set then
+                card_type = G.P_CENTERS[most_used_key].set
+            end
+
+            if #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
+                G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
+                G.E_MANAGER:add_event(Event({
+                    trigger = 'after',
+                    delay = 0.4,
+                    func = function()
+                        local new_card = create_card(card_type, G.consumeables, nil, nil, nil, nil, most_used_key, 'pattern_voucher')
+                        new_card:add_to_deck()
+                        G.consumeables:emplace(new_card)
+                        G.GAME.consumeable_buffer = G.GAME.consumeable_buffer - 1
+                        return true
+                    end
+                }))
+            end
+        end
+    end,
+})
+
+SMODS.Voucher({
+    key = "tesselation",
+    atlas = "AbandoniaVouchers",
+    pos = {
+        x = 4,
+        y = 3,
+    },
+    cost = 10,
+    requires = {"v_abn_pattern"},
+
+    calculate = function(self, card, context)
+        if context.setting_blind then
+            local first_key, second_key = nil, nil
+            local first_uses, second_uses = -1, -1
+
+            local profile_stats = nil
+            if G.PROFILES and G.SETTINGS and G.SETTINGS.profile and G.PROFILES[G.SETTINGS.profile] then
+                profile_stats = G.PROFILES[G.SETTINGS.profile].consumeable_usage
+            elseif G.PROFILE then
+                profile_stats = G.PROFILE.consumeable_usage
+            end
+
+            for _, center in pairs(G.P_CENTERS) do
+                if center.consumeable then
+                    local curr_uses = 0
+                    if profile_stats and profile_stats[center.key] then
+                        curr_uses = profile_stats[center.key].count or profile_stats[center.key] or 0
+                    end
+
+                    if curr_uses > first_uses then
+                        second_uses = first_uses
+                        second_key = first_key
+                        first_uses = curr_uses
+                        first_key = center.key
+                    elseif curr_uses > second_uses then
+                        second_uses = curr_uses
+                        second_key = center.key
+                    end
+                end
+            end
+
+            if not first_key or first_uses <= 0 then
+                first_key = "c_fool"
+            end
+            if not second_key or second_uses <= 0 then
+                second_key = first_key
+            end
+
+            local keys_to_spawn = {first_key, second_key}
+
+            for _, key in ipairs(keys_to_spawn) do
+                local card_type = "Tarot"
+                if G.P_CENTERS[key] and G.P_CENTERS[key].set then
+                    card_type = G.P_CENTERS[key].set
+                end
+
+                if #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
+                    G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
+                    G.E_MANAGER:add_event(Event({
+                        trigger = 'after',
+                        delay = 0.4,
+                        func = function()
+                            local new_card = create_card(card_type, G.consumeables, nil, nil, nil, nil, key, 'tesselation_voucher')
+                            new_card:add_to_deck()
+                            G.consumeables:emplace(new_card)
+                            G.GAME.consumeable_buffer = G.GAME.consumeable_buffer - 1
+                            return true
+                        end
+                    }))
+                end
+            end
+        end
+    end,
+})
+
+SMODS.Voucher({
+    key = "silver_spoon",
+    atlas = "AbandoniaVouchers",
+    pos = {
+        x = 5,
+        y = 3,
+    },
+    cost = 10,
+    
+    redeem = function(self, card)
+        G.GAME.silver_spoon_used = false
+    end,
+
+    calculate = function(self, card, context)
+        local is_valid_purchase = (context.buying_card and context.card ~= card) or context.open_booster
+        
+        if is_valid_purchase and not G.GAME.silver_spoon_used then
+            local target_item = context.card or context.to_open
+            
+            if target_item and target_item.config and target_item.config.center and target_item.config.center.key == "v_abn_silver_spoon" then
+                return
+            end
+
+            local cost = target_item and target_item.cost or 0
+            
+            if cost > 0 then
+                G.GAME.silver_spoon_used = true
+                
+                local refund = math.floor(cost * 0.5)
+                if refund > 0 then
+                    ease_dollars(refund)
+                    card:juice_up(0.3, 0.3)
+                end
+            end
+        end
+        
+        if context.starting_shop then
+            G.GAME.silver_spoon_used = false
+        end
+    end,
+})
+
+SMODS.Voucher({
+    key = "heirloom",
+    atlas = "AbandoniaVouchers",
+    pos = {
+        x = 6,
+        y = 3,
+    },
+    cost = 10,
+    requires = { "v_abn_silver_spoon" },
+    
+    redeem = function(self, card)
+        G.GAME.heirloom_ready = true
+    end,
+
+    calculate = function(self, card, context)
+        local is_valid_purchase = (context.buying_card and context.card ~= card) or context.open_booster
+        
+        if is_valid_purchase and G.GAME.heirloom_ready then
+            local target_item = context.card or context.to_open
+            
+            if target_item and target_item.config and target_item.config.center and target_item.config.center.key == "v_abn_heirloom" then
+                return
+            end
+
+            ease_dollars(2)
+        end
+        
+        if context.starting_shop then
+            G.GAME.heirloom_ready = true
+        end
+		
+		if context.ending_shop then
+			G.GAME.heirloom_ready = false
+		end
+    end,
+})
