2864f88025a5e5b0f5246d9d30cd29f383e9d459
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-09-13 22:15:36 +0100

    fah
    ok so the same problem exists for seals...
    
    Ok so I was originally gonna have SEALS, in seperate files
           but for some reason the localisation doesn't load there??? It only works in the main loc
           which I find kinda weird, so :p

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -178,7 +178,7 @@ return {
 
     Other = {
       --[[
-       Ok so I was originally gonna have Stickers, Stamps and Enhancement Stickers in seperate files
+       Ok so I was originally gonna have Seals, Stickers, Stamps and Enhancement Stickers in seperate files
        but for some reason the localisation doesn't load there??? It only works in the main loc
        which I find kinda weird, so :p
        ]]
@@ -359,6 +359,164 @@ return {
         }
       },
       --#endregion
+
+
+      --#region Seals
+      abn_duality_seal = {
+        name = "Duality Seal",
+        text = {
+          "This card permanently gains",
+          "{C:chips}+#1#{} Chips and {C:mult}+#2#{} Mult",
+          "per {C:abn_duality_seal}Duality Seal{} held"
+        }
+      },
+      abn_rainbow_seal = {
+        name = "Rainbow Seal",
+        text = {
+          "{C:gold}+#1#{} Ascension Power",
+          "when scored"
+        }
+      },
+      abn_silver_seal = {
+        name = "Silver Seal",
+        text = {
+          "Creates a {C:weather_report}Weather Report{}",
+          "when {C:attention}discarded",
+          "{C:inactive}(Must have room)",
+        },
+      },
+      abn_copper_seal = {
+        name = "Copper Seal",
+        text = {
+          "Creates a {C:program_pack}Program{} card",
+          "when {C:attention}discarded",
+          "{C:inactive}(Must have room)",
+        },
+      },
+      abn_linguistic_seal = {
+        name = "Linguistic Seal",
+        text = {
+          "Creates a {C:lexica}Lexica{} card",
+          "when {C:attention}discarded",
+          "{C:inactive}(Must have room)",
+        },
+      },
+      abn_skyblue_seal = {
+        name = "Skyblue Seal",
+        text = {
+          "Counts in scoring when",
+          "{C:attention}held{} in hand"
+        }
+      },
+      abn_brine_seal = {
+        name = "Brine Seal",
+        text = {
+          "Gains {C:chips}+#2#{} chips",
+          "when {C:attention}discarded",
+          "{C:inactive}(Currently {C:chips}+#1#{C:inactive} Chips)"
+        }
+      },
+      abn_oxidized_seal = {
+        name = "Oxidised Seal",
+        text = {
+          "Creates a {C:nightshift_cards}Nightshift{} card",
+          "when {C:red}destroyed",
+          "{C:inactive}(Must have room)",
+        }
+      },
+      abn_brass_seal = {
+        name = "Brass Seal",
+        text = {
+          "Retrigger {C:attention}playing card{}",
+          "to the {C:attention}right{} when scored"
+        }
+      },
+      abn_lavender_seal = {
+        name = "Lavender Seal",
+        text = {
+          "Retrigger {C:attention}playing card{}",
+          "to the {C:attention}left{} when scored"
+        }
+      },
+      abn_violet_seal = {
+        name = "Violet Seal",
+        text = {
+          "{C:white,X:chips}X#1#{} Chips"
+        }
+      },
+      abn_brown_seal = {
+        name = "Brown Seal",
+        text = {
+          "{C:white,X:mult}X#1#{} Mult"
+        }
+      },
+      abn_white_seal = {
+        name = "White Seal",
+        text = {
+          "Does not count",
+          "towards {C:red}discard{}"
+        }
+      },
+      abn_teal_seal = {
+        name = "Teal Seal",
+        text = {
+          "Creates a {C:abn_calamity}Calamity{} card",
+          "if {C:attention}held{} in hand",
+          "at end of round",
+          "{C:inactive}(Must have room)",
+        }
+      },
+      abn_black_seal = {
+        name = "Black Seal",
+        text = {
+          "Creates a {C:sigils}Sigil{} card",
+          "when {C:attention}discarded",
+          "{C:inactive}(Must have room)",
+        },
+      },
+      abn_orange_seal = {
+        name = "Orange Seal",
+        text = {
+          "Creates the {C:planet}Planet{} card",
+          "for this card's {C:attention}rank{}",
+          "if {C:attention}held{} in hand",
+          "at end of round",
+          "{C:inactive}(Must have room)",
+        }
+      },
+      abn_pink_seal = {
+        name = "Pink Seal",
+        text = {
+          "Creates a {C:astro_cards}Astro{} card",
+          "when {C:attention}discarded",
+          "{C:inactive}(Must have room)",
+        },
+      },
+      abn_lime_seal = {
+        name = "Lime Seal",
+        text = {
+          "Creates a {C:calligraphy}Calligraphy{} card",
+          "when {C:attention}discarded",
+          "{C:inactive}(Must have room)",
+        },
+      },
+      abn_grey_seal = {
+        name = "Grey Seal",
+        text = {
+          "Earn {C:money}$#1#{}",
+          "when {C:attention}discarded",
+        }
+      },
+      abn_rubicund_seal = {
+        name = "Rubicund Seal",
+        text = {
+          "Gains {C:mult}+#2#{} Mult",
+          "when {C:attention}discarded",
+          "{C:inactive}(Currently {C:mult}+#1#{C:inactive} Mult)"
+        },
+      },
+      --#endregion
+
       --#region stickers
       abn_pump_up = {
         name = "Pump Up",
@@ -1485,6 +1643,28 @@ return {
       algebraic = "Algebraic",
       illusion = "Illusion",
       ram = "RAM",
+
+      -- Seals
+      abn_orange_seal = "Orange Seal",
+      abn_grey_seal = "Grey Seal",
+      abn_pink_seal = "Pink Seal",
+      abn_black_seal = "Black Seal",
+      abn_teal_seal = "Teal Seal",
+      abn_white_seal = "White Seal",
+      abn_brown_seal = "Brown Seal",
+      abn_violet_seal = "Violet Seal",
+      abn_lavender_seal = "Lavender Seal",
+      abn_skyblue_seal = "Skyblue Seal",
+      abn_oxidized_seal = "Oxidised Seal",
+      abn_brass_seal = "Brass Seal",
+      abn_copper_seal = "Copper Seal",
+      abn_silver_seal = "Silver Seal",
+      abn_linguistic_seal = "Linguistic Seal",
+      abn_rubicund_seal = "Rubicund Seal",
+      abn_brine_seal = "Brine Seal",
+      abn_rainbow_seal = "Rainbow Seal",
+      abn_duality_seal = "Duality Seal",
+      abn_lime_seal = "Lime Seal",
 
       -- Stickers
       abn_pump_up = "Pump Up",
