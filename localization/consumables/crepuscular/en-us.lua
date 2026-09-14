51e0957f9f61e7d39320d4ba20ec830e486f9e6f
EricTheToon <EricTheToon@gmail.com>
2026-09-13 19:43:16 -0500

    Add files via upload

---
+++ localization/consumables/crepuscular/en-us.lua
@@ -0,0 +1,20 @@
+return {
+    descriptions = {
+        crepuscular = {
+            c_abn_sailboat = {
+                name = "Sailboat",
+                text = {
+                    "Select up to {C:attention}#1#{} cards",
+                    "to turn into {C:attention}#2#s{}",
+                },
+            },
+            c_abn_stone_of_destiny = {
+                name = "Stone Of Destiny",
+                text = {
+                    "Select up to {C:attention}#1#{} cards",
+                    "to turn into {C:attention}#2#s{}",
+                },
+            },
+        },
+    },
+}
