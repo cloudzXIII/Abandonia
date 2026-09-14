75fe846bd3bb5af1d3597f760942e53579ac2086
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-09-14 17:12:39 +0100

    fix crepuscular tarot sprite positions

--- content/misc/crepuscular.lua
+++ content/misc/crepuscular.lua
@@ -8,33 +8,33 @@ SMODS.ConsumableType {
 }
 
 SMODS.Consumable {
-    key = "sailboat",
-    set = 'crepuscular',
-    cost = 4,
-    atlas = "AbandoniaCrepuscular",
-    pos = { x = 0, y = 1 },
-    config = { max_highlighted = 2, mod_conv = 'm_abn_ocean' },
-    loc_vars = function(self, info_queue, card)
-        info_queue[#info_queue + 1] = G.P_CENTERS[card.ability.mod_conv]
-        return { vars = { card.ability.max_highlighted, localize { type = 'name_text', set = 'Enhanced', key = card.ability.mod_conv } } }
-    end,
-    abn_artist_credits = {
-        artist = "SmoliconBoi",
-    },
+  key = "sailboat",
+  set = 'crepuscular',
+  cost = 4,
+  atlas = "AbandoniaCrepuscular",
+  pos = { x = 1, y = 0 },
+  config = { max_highlighted = 2, mod_conv = 'm_abn_ocean' },
+  loc_vars = function(self, info_queue, card)
+    info_queue[#info_queue + 1] = G.P_CENTERS[card.ability.mod_conv]
+    return { vars = { card.ability.max_highlighted, localize { type = 'name_text', set = 'Enhanced', key = card.ability.mod_conv } } }
+  end,
+  abn_artist_credits = {
+    artist = "SmoliconBoi",
+  },
 }
 
 SMODS.Consumable {
-    key = "stone_of_destiny",
-    set = 'crepuscular',
-    cost = 4,
-    atlas = "AbandoniaCrepuscular",
-    pos = { x = 2, y = 1 },
-    config = { max_highlighted = 2, mod_conv = 'm_abn_kinship' },
-    loc_vars = function(self, info_queue, card)
-        info_queue[#info_queue + 1] = G.P_CENTERS[card.ability.mod_conv]
-        return { vars = { card.ability.max_highlighted, localize { type = 'name_text', set = 'Enhanced', key = card.ability.mod_conv } } }
-    end,
-    abn_artist_credits = {
-        artist = "SmoliconBoi",
-    },
+  key = "stone_of_destiny",
+  set = 'crepuscular',
+  cost = 4,
+  atlas = "AbandoniaCrepuscular",
+  pos = { x = 2, y = 0 },
+  config = { max_highlighted = 2, mod_conv = 'm_abn_kinship' },
+  loc_vars = function(self, info_queue, card)
+    info_queue[#info_queue + 1] = G.P_CENTERS[card.ability.mod_conv]
+    return { vars = { card.ability.max_highlighted, localize { type = 'name_text', set = 'Enhanced', key = card.ability.mod_conv } } }
+  end,
+  abn_artist_credits = {
+    artist = "SmoliconBoi",
+  },
 }
