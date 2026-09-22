7be0de64dee78a875613763a7b5ea0c8e6b7195d
IrisCantArt <souperwolf3@gmail.com>
2026-09-21 23:00:28 -0400

    another new card+a fix to my other card (=-ω-=)

--- content/cards/liam.lua
+++ content/cards/liam.lua
@@ -1,10 +1,10 @@
 SMODS.Joker{
-  key = "Lorenz",
+  key = "Liam",
 
   atlas = 'AbandoniaLegendary',
-  pos = { x = 4, y = 19 },
-  soul_pos = { x = 5, y = 19 },
-  cost = 20, 
+  pos = { x = 6, y = 19 },
+  soul_pos = { x = 7, y = 19 },
+  cost = 20,
   rarity = 4,
   discovered = false,
   blueprint_compat = true,
@@ -12,18 +12,17 @@ SMODS.Joker{
 
   config = {
     extra = {
-      xmult = 1.5,
+      xchips = 1.5,
       gain = 0.04
     }
   },
 
-
   loc_vars = function(self, info_queue, card)
     return {
       vars = {
-        card.ability.extra.xmult,
+        card.ability.extra.xchips,
         card.ability.extra.gain,
-        0.25
+        100
       }
     }
   end,
@@ -31,24 +30,24 @@ SMODS.Joker{
   calculate = function(self, card, context)
     if context.individual and context.cardarea == G.play and context.other_card then
       if context.other_card.base.suit == "Acorn" or context.other_card.base.suit == "abn_Acorn" then
-        card.ability.extra.xmult = card.ability.extra.xmult + card.ability.extra.gain
+        card.ability.extra.xchips = card.ability.extra.xchips + card.ability.extra.gain
 
-        local chips = 0
+        local score = 0
 
         if G.jokers then
           for _, joker in ipairs(G.jokers.cards) do
             if joker ~= card and joker.config and joker.config.center then
               local rarity = joker.config.center.rarity
               if type(rarity) == "number" then
-                chips = chips + (0.25 * rarity)
+                score = score + (100 * rarity)
               end
             end
           end
         end
 
         return {
-          x_mult = card.ability.extra.xmult,
-          chips = chips,
+          x_chips = card.ability.extra.xchips,
+          chip_mod = score,
         }
       end
     end
