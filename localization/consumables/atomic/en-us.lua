5ceac96e6f344480309f7717d0c7d34f5e2dcf4b
EricTheToon <EricTheToon@gmail.com>
2026-09-24 16:45:42 -0500

    Add files via upload

---
+++ localization/consumables/atomic/en-us.lua
@@ -0,0 +1,21 @@
+return {
+    descriptions = {
+        atomic = {
+            c_abn_1h = {
+                name = "1H",
+                text = {
+                    "{C:attention}#1#{} random cards",
+					"{C:attention}reduce{} their {C:chips}Chips{} to {C:chips}1{}",
+					"and gain {X:mult,C:white}X#2#{} Mult",
+                },
+            },
+            c_abn_stone_of_destiny = {
+                name = "Stone Of Destiny",
+                text = {
+                    "Select up to {C:attention}#1#{} cards",
+                    "to turn into {C:attention}#2#s{}",
+                },
+            },
+        },
+    },
+}
