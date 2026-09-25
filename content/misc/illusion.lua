3eba7a699861afaba6bc0d9c1741ba3cbdfab6e1
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-09-25 11:55:53 +0100

    illusion undiscovered sprite

--- content/misc/illusion.lua
+++ content/misc/illusion.lua
@@ -6,269 +6,275 @@ SMODS.ConsumableType {
   shop_rate = 0,
 }
 
-SMODS.Consumable {
-    key = "euclidean",
-    set = 'illusion',
-    cost = 4,
-    atlas = "abn_AbandoniaIllusions",
-    pos = { x = 0, y = 0 },
-    config = { extra = { cards = 1, odds = 6 } },
-    loc_vars = function(self, info_queue, card)
-		info_queue[#info_queue + 1] = G.P_CENTERS.c_abn_penrose
-        return { vars = { card.ability.extra.cards, G.GAME.probabilities.normal, card.ability.extra.odds } }
-    end,
-
-    can_use = function(self, card)
-        if G and G.hand and G.hand.highlighted and card.ability and card.ability.extra and card.ability.extra.cards then
-            if #G.hand.highlighted > 0 and #G.hand.highlighted <= card.ability.extra.cards then
-                return true
-            end
-        end
-        return false
-    end,
-
-    use = function(self, card, area, copier)
-        if G and G.hand and G.hand.highlighted then
-            for i = 1, #G.hand.highlighted do
-                local c = G.hand.highlighted[i]
-                abn_add_stamp(c, "abn_empty_stamp")
-            end
-        end
-
-        if pseudorandom('euclidean') < G.GAME.probabilities.normal / card.ability.extra.odds then
-            G.E_MANAGER:add_event(Event({
-                trigger = 'after',
-                delay = 0.4,
-                func = function()
-					play_sound('timpani')
-                    local c = create_card('illusion', G.consumeables, nil, nil, nil, nil, 'c_abn_penrose', 'euclid')
-                    c:add_to_deck()
-                    G.consumeables:emplace(c)
-                    return true
-                end
-            }))
-        else
-            G.E_MANAGER:add_event(Event({
-                trigger = 'after',
-                delay = 0.4,
-                func = function()
-                    card_eval_status_text(card, 'extra', nil, nil, nil, { message = localize('k_nope_ex'), colour = G.C.SECONDARY_SET.Tarot })
-                    return true
-                end
-            }))
-        end
-    end,
-
-    abn_artist_credits = {
-        artist = "0kronix"
-    },
+SMODS.UndiscoveredSprite {
+  key = 'illusion',
+  atlas = 'abn_AbandoniaUndiscovered',
+  pos = { x = 4, y = 3 },
 }
 
 SMODS.Consumable {
-    key = "penrose",
-    set = 'illusion',
-    cost = 4,
-    atlas = "abn_AbandoniaIllusions",
-    pos = { x = 0, y = 1 }, 
-    config = {},
-	hidden = true,
-    soul_set = "illusion",
-    soul_rate = 0,
-    loc_vars = function(self, info_queue, card)
-        info_queue[#info_queue + 1] = { key = "abn_empty_stamp", set = "Other", vars = {} }
-        return { vars = {} }
-    end,
+  key = "euclidean",
+  set = 'illusion',
+  cost = 4,
+  atlas = "abn_AbandoniaIllusions",
+  pos = { x = 0, y = 0 },
+  config = { extra = { cards = 1, odds = 6 } },
+  loc_vars = function(self, info_queue, card)
+    info_queue[#info_queue + 1] = G.P_CENTERS.c_abn_penrose
+    return { vars = { card.ability.extra.cards, G.GAME.probabilities.normal, card.ability.extra.odds } }
+  end,
 
-    can_use = function(self, card)
-        if G and G.hand and G.hand.cards then
-            return #G.hand.cards > 0
+  can_use = function(self, card)
+    if G and G.hand and G.hand.highlighted and card.ability and card.ability.extra and card.ability.extra.cards then
+      if #G.hand.highlighted > 0 and #G.hand.highlighted <= card.ability.extra.cards then
+        return true
+      end
+    end
+    return false
+  end,
+
+  use = function(self, card, area, copier)
+    if G and G.hand and G.hand.highlighted then
+      for i = 1, #G.hand.highlighted do
+        local c = G.hand.highlighted[i]
+        abn_add_stamp(c, "abn_empty_stamp")
+      end
+    end
+
+    if pseudorandom('euclidean') < G.GAME.probabilities.normal / card.ability.extra.odds then
+      G.E_MANAGER:add_event(Event({
+        trigger = 'after',
+        delay = 0.4,
+        func = function()
+          play_sound('timpani')
+          local c = create_card('illusion', G.consumeables, nil, nil, nil, nil, 'c_abn_penrose', 'euclid')
+          c:add_to_deck()
+          G.consumeables:emplace(c)
+          return true
         end
-        return false
-    end,
-
-    use = function(self, card, area, copier)
-
-        if G and G.hand and G.hand.cards then
-            for i = 1, #G.hand.cards do
-                local c = G.hand.cards[i]
-                abn_add_stamp(c, "abn_empty_stamp")
-            end
+      }))
+    else
+      G.E_MANAGER:add_event(Event({
+        trigger = 'after',
+        delay = 0.4,
+        func = function()
+          card_eval_status_text(card, 'extra', nil, nil, nil,
+            { message = localize('k_nope_ex'), colour = G.C.SECONDARY_SET.Tarot })
+          return true
         end
-    end,
+      }))
+    end
+  end,
 
-    abn_artist_credits = {
-        artist = "Grummy"
-    },
+  abn_artist_credits = {
+    artist = "0kronix"
+  },
 }
 
 SMODS.Consumable {
-    key = "sword_box",
-    set = 'illusion',
-    cost = 4,
-    atlas = "abn_AbandoniaIllusions",
-    pos = { x = 2, y = 0 },
-    config = { extra = { cards = 1, } },
-    loc_vars = function(self, info_queue, card)
-		info_queue[#info_queue + 1] = { key = "abn_jester_stamp", set = "Other", vars = {} }
-        return { vars = { card.ability.extra.cards, } }
-    end,
+  key = "penrose",
+  set = 'illusion',
+  cost = 4,
+  atlas = "abn_AbandoniaIllusions",
+  pos = { x = 0, y = 1 },
+  config = {},
+  hidden = true,
+  soul_set = "illusion",
+  soul_rate = 0,
+  loc_vars = function(self, info_queue, card)
+    info_queue[#info_queue + 1] = { key = "abn_empty_stamp", set = "Other", vars = {} }
+    return { vars = {} }
+  end,
 
-    can_use = function(self, card)
-        if G and G.hand and G.hand.highlighted and card.ability and card.ability.extra and card.ability.extra.cards then
-            if #G.hand.highlighted > 0 and #G.hand.highlighted <= card.ability.extra.cards then
-                return true
-            end
-        end
-        return false
-    end,
+  can_use = function(self, card)
+    if G and G.hand and G.hand.cards then
+      return #G.hand.cards > 0
+    end
+    return false
+  end,
 
-    use = function(self, card, area, copier)
-        if G and G.hand and G.hand.highlighted then
-            for i = 1, #G.hand.highlighted do
-                local c = G.hand.highlighted[i]
-                abn_add_stamp(c, "abn_jester_stamp")
-            end
-        end
-    end,
+  use = function(self, card, area, copier)
+    if G and G.hand and G.hand.cards then
+      for i = 1, #G.hand.cards do
+        local c = G.hand.cards[i]
+        abn_add_stamp(c, "abn_empty_stamp")
+      end
+    end
+  end,
 
-    abn_artist_credits = {
-        artist = "0kronix"
-    },
+  abn_artist_credits = {
+    artist = "Grummy"
+  },
 }
 
 SMODS.Consumable {
-    key = "illusionist",
-    set = 'illusion',
-    cost = 4,
-    atlas = "abn_AbandoniaIllusions",
-    pos = { x = 3, y = 0 },
-    config = { extra = { cards = 1, } },
-    loc_vars = function(self, info_queue, card)
-		info_queue[#info_queue + 1] = { key = "abn_icon_stamp", set = "Other", vars = { 1 } }
-        return { vars = { card.ability.extra.cards, } }
-    end,
+  key = "sword_box",
+  set = 'illusion',
+  cost = 4,
+  atlas = "abn_AbandoniaIllusions",
+  pos = { x = 2, y = 0 },
+  config = { extra = { cards = 1, } },
+  loc_vars = function(self, info_queue, card)
+    info_queue[#info_queue + 1] = { key = "abn_jester_stamp", set = "Other", vars = {} }
+    return { vars = { card.ability.extra.cards, } }
+  end,
 
-    can_use = function(self, card)
-        if G and G.hand and G.hand.highlighted and card.ability and card.ability.extra and card.ability.extra.cards then
-            if #G.hand.highlighted > 0 and #G.hand.highlighted <= card.ability.extra.cards then
-                return true
-            end
-        end
-        return false
-    end,
+  can_use = function(self, card)
+    if G and G.hand and G.hand.highlighted and card.ability and card.ability.extra and card.ability.extra.cards then
+      if #G.hand.highlighted > 0 and #G.hand.highlighted <= card.ability.extra.cards then
+        return true
+      end
+    end
+    return false
+  end,
 
-    use = function(self, card, area, copier)
-        if G and G.hand and G.hand.highlighted then
-            for i = 1, #G.hand.highlighted do
-                local c = G.hand.highlighted[i]
-                abn_add_stamp(c, "abn_icon_stamp")
-            end
-        end
-    end,
+  use = function(self, card, area, copier)
+    if G and G.hand and G.hand.highlighted then
+      for i = 1, #G.hand.highlighted do
+        local c = G.hand.highlighted[i]
+        abn_add_stamp(c, "abn_jester_stamp")
+      end
+    end
+  end,
 
-    abn_artist_credits = {
-        artist = "0kronix"
-    },
+  abn_artist_credits = {
+    artist = "0kronix"
+  },
 }
 
 SMODS.Consumable {
-    key = "cotard",
-    set = 'illusion',
-    cost = 4,
-    atlas = "abn_AbandoniaIllusions",
-    pos = { x = 3, y = 1 },
-    config = { extra = { cards = 1, } },
-    loc_vars = function(self, info_queue, card)
-		info_queue[#info_queue + 1] = { key = "abn_psychomancy_stamp", set = "Other", vars = {} }
-        return { vars = { card.ability.extra.cards, } }
-    end,
+  key = "illusionist",
+  set = 'illusion',
+  cost = 4,
+  atlas = "abn_AbandoniaIllusions",
+  pos = { x = 3, y = 0 },
+  config = { extra = { cards = 1, } },
+  loc_vars = function(self, info_queue, card)
+    info_queue[#info_queue + 1] = { key = "abn_icon_stamp", set = "Other", vars = { 1 } }
+    return { vars = { card.ability.extra.cards, } }
+  end,
 
-    can_use = function(self, card)
-        if G and G.hand and G.hand.highlighted and card.ability and card.ability.extra and card.ability.extra.cards then
-            if #G.hand.highlighted > 0 and #G.hand.highlighted <= card.ability.extra.cards then
-                return true
-            end
-        end
-        return false
-    end,
+  can_use = function(self, card)
+    if G and G.hand and G.hand.highlighted and card.ability and card.ability.extra and card.ability.extra.cards then
+      if #G.hand.highlighted > 0 and #G.hand.highlighted <= card.ability.extra.cards then
+        return true
+      end
+    end
+    return false
+  end,
 
-    use = function(self, card, area, copier)
-        if G and G.hand and G.hand.highlighted then
-            for i = 1, #G.hand.highlighted do
-                local c = G.hand.highlighted[i]
-                abn_add_stamp(c, "abn_psychomancy_stamp")
-            end
-        end
-    end,
+  use = function(self, card, area, copier)
+    if G and G.hand and G.hand.highlighted then
+      for i = 1, #G.hand.highlighted do
+        local c = G.hand.highlighted[i]
+        abn_add_stamp(c, "abn_icon_stamp")
+      end
+    end
+  end,
 
-    abn_artist_credits = {
-        artist = "Dogg-Fly"
-    },
+  abn_artist_credits = {
+    artist = "0kronix"
+  },
 }
 
 SMODS.Consumable {
-    key = "misdirection",
-    set = 'illusion',
-    cost = 4,
-    atlas = "abn_AbandoniaIllusions",
-    pos = { x = 4, y = 0 },
-    config = { extra = { cards = 1, } },
-    loc_vars = function(self, info_queue, card)
-		info_queue[#info_queue + 1] = { key = "abn_prehensile_stamp", set = "Other", vars = {} }
-        return { vars = { card.ability.extra.cards, } }
-    end,
+  key = "cotard",
+  set = 'illusion',
+  cost = 4,
+  atlas = "abn_AbandoniaIllusions",
+  pos = { x = 3, y = 1 },
+  config = { extra = { cards = 1, } },
+  loc_vars = function(self, info_queue, card)
+    info_queue[#info_queue + 1] = { key = "abn_psychomancy_stamp", set = "Other", vars = {} }
+    return { vars = { card.ability.extra.cards, } }
+  end,
 
-    can_use = function(self, card)
-        if G and G.hand and G.hand.highlighted and card.ability and card.ability.extra and card.ability.extra.cards then
-            if #G.hand.highlighted > 0 and #G.hand.highlighted <= card.ability.extra.cards then
-                return true
-            end
-        end
-        return false
-    end,
+  can_use = function(self, card)
+    if G and G.hand and G.hand.highlighted and card.ability and card.ability.extra and card.ability.extra.cards then
+      if #G.hand.highlighted > 0 and #G.hand.highlighted <= card.ability.extra.cards then
+        return true
+      end
+    end
+    return false
+  end,
 
-    use = function(self, card, area, copier)
-        if G and G.hand and G.hand.highlighted then
-            for i = 1, #G.hand.highlighted do
-                local c = G.hand.highlighted[i]
-                abn_add_stamp(c, "abn_prehensile_stamp")
-            end
-        end
-    end,
+  use = function(self, card, area, copier)
+    if G and G.hand and G.hand.highlighted then
+      for i = 1, #G.hand.highlighted do
+        local c = G.hand.highlighted[i]
+        abn_add_stamp(c, "abn_psychomancy_stamp")
+      end
+    end
+  end,
 
-    abn_artist_credits = {
-        artist = "Astellar Blue"
-    },
+  abn_artist_credits = {
+    artist = "Dogg-Fly"
+  },
 }
 
 SMODS.Consumable {
-    key = "god_complex",
-    set = 'illusion',
-    cost = 4,
-    atlas = "abn_AbandoniaIllusions",
-    pos = { x = 1, y = 1 },
-	hidden = true,
-	soul_set = 'illusion',
-	soul_rate = 0.035,
-    loc_vars = function(self, info_queue, card)
-    	info_queue[#info_queue + 1] = { key = "abn_newestia_only", set = "Other" }
-    end,
-    can_use = function(self, card)
-        for _, playing_card in ipairs(G.playing_cards) do
-			if playing_card.edition then
-				return true
-			end
-		end
-        return false
-    end,
-    use = function(self, card, area, copier)
-        for _, playing_card in ipairs(G.playing_cards) do
-			if playing_card.edition then
-				abn_add_stamp(playing_card)
-			end
-		end
-    end,
-    abn_artist_credits = {
-        artist = "Grummy"
-    },
+  key = "misdirection",
+  set = 'illusion',
+  cost = 4,
+  atlas = "abn_AbandoniaIllusions",
+  pos = { x = 4, y = 0 },
+  config = { extra = { cards = 1, } },
+  loc_vars = function(self, info_queue, card)
+    info_queue[#info_queue + 1] = { key = "abn_prehensile_stamp", set = "Other", vars = {} }
+    return { vars = { card.ability.extra.cards, } }
+  end,
+
+  can_use = function(self, card)
+    if G and G.hand and G.hand.highlighted and card.ability and card.ability.extra and card.ability.extra.cards then
+      if #G.hand.highlighted > 0 and #G.hand.highlighted <= card.ability.extra.cards then
+        return true
+      end
+    end
+    return false
+  end,
+
+  use = function(self, card, area, copier)
+    if G and G.hand and G.hand.highlighted then
+      for i = 1, #G.hand.highlighted do
+        local c = G.hand.highlighted[i]
+        abn_add_stamp(c, "abn_prehensile_stamp")
+      end
+    end
+  end,
+
+  abn_artist_credits = {
+    artist = "Astellar Blue"
+  },
+}
+
+SMODS.Consumable {
+  key = "god_complex",
+  set = 'illusion',
+  cost = 4,
+  atlas = "abn_AbandoniaIllusions",
+  pos = { x = 1, y = 1 },
+  hidden = true,
+  soul_set = 'illusion',
+  soul_rate = 0.035,
+  loc_vars = function(self, info_queue, card)
+    info_queue[#info_queue + 1] = { key = "abn_newestia_only", set = "Other" }
+  end,
+  can_use = function(self, card)
+    for _, playing_card in ipairs(G.playing_cards) do
+      if playing_card.edition then
+        return true
+      end
+    end
+    return false
+  end,
+  use = function(self, card, area, copier)
+    for _, playing_card in ipairs(G.playing_cards) do
+      if playing_card.edition then
+        abn_add_stamp(playing_card)
+      end
+    end
+  end,
+  abn_artist_credits = {
+    artist = "Grummy"
+  },
 }
