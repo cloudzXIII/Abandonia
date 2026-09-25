b91d45bcf5ffbecefec86a205083603c2af3341b
EricTheToon <EricTheToon@gmail.com>
2026-09-24 21:01:28 -0500

    Add files via upload

--- localization/consumables/atomic/en-us.lua
+++ localization/consumables/atomic/en-us.lua
@@ -9,11 +9,71 @@ return {
 					"and gain {X:mult,C:white}X#2#{} Mult",
                 },
             },
-            c_abn_stone_of_destiny = {
-                name = "Stone Of Destiny",
+            c_abn_d2h = {
+                name = "D2H",
                 text = {
-                    "Select up to {C:attention}#1#{} cards",
-                    "to turn into {C:attention}#2#s{}",
+                    "{C:attention}#1#{} random cards",
+                    "gain {C:gold}+#2#{} Asc. Power",
+					"per {C:planet}Planet{} {C:attention}Rank{} they have"
+                },
+            },
+			c_abn_2he = {
+                name = "2He",
+                text = {
+                    "{C:attention}#1#{} random cards",
+                    "gain {C:purple}+#2#{} score",
+                },
+            },
+			c_abn_li3 = {
+                name = "Li3",
+                text = {
+                    "{C:attention}#1#{} random cards",
+                    "gain {{X:chips,C:white}X#2#{} Chips",
+                },
+            },
+			c_abn_be4 = {
+                name = "Be4",
+                text = {
+                    "Create a copy of",
+					"{C:attention}#1#{} random cards",
+                },
+            },
+			c_abn_5b = {
+                name = "5B",
+                text = {
+                    "{C:attention}#1#{} random cards",
+					"have their {C:chips}Chips{} {C:attention}Tripled{}",
+                },
+            },
+			c_abn_6c = {
+                name = "6C",
+                text = {
+                    "{C:attention}#1#{} random cards",
+					"gain {C:mult}Mult{} equal to",
+					"{C:attention}double{} their {C:attention}rank{}",
+                },
+            },
+			c_abn_7n = {
+                name = "7N",
+                text = {
+                    "{C:attention}#1#{} random cards",
+					"gain the {C:attention}seals{} and {C:dark_edition}editions{} of",
+					"{C:attention}#1#{} other random cards",
+                },
+            },
+			c_abn_8o = {
+                name = "8O",
+                text = {
+                    "{C:attention}#1#{} random cards",
+					"gain {C:mult}+#2#{} Mult and {C:chips}+#3#{} Chips",
+                },
+            },
+			c_abn_9f = {
+                name = "9F",
+                text = {
+                    "{C:attention}#1#{} random cards",
+					"gain {C:attention}double{} the {C:chips}chip{} value",
+					"of {C:attention}#1#{} other random cards",
                 },
             },
         },
